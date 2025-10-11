35a3e456063e995fadc674544f1ae3cb21475308
vliu36 <116984041+vliu36@users.noreply.github.com>
2025-10-10 17:52:37 -0700

    implemented prefix search

--- server/controllers/communities.ts
+++ server/controllers/communities.ts
@@ -21,6 +21,33 @@ const getAllDocuments = async (req: Request, res: Response) => {
     }    
 }
 
+// Retrieve documents in Communities that contain the prefix value
+const prefixSearch = async (req: Request, res: Response) => {
+    try {
+        const query = req.params.query;
+        if (!query) {
+            res.status(400).send({
+                status: "Bad Request",
+                message: "Empty query!"
+            })
+        }
+        
+        const communitiesRef = db.collection("Communities").where("name", ">=", query).where("name", "<=", query + "\uf8ff");
+        const snapshot = await communitiesRef.get();
+        
+        res.status(200).send({
+            status: "OK",
+            message: snapshot.docs.map(doc => doc.data())
+        });
+    }
+    catch (err) {
+        res.status(500).send({
+            status: "Backend error",
+            message: err
+        })
+    }
+}
 export {
-    getAllDocuments
+    getAllDocuments,
+    prefixSearch
 }
--- server/routes/communities.ts
+++ server/routes/communities.ts
@@ -6,6 +6,12 @@ const router = express.Router();
 /** Retrieve all documents in Communities
  *  @route GET /api/comm/all
  */
-router.get("/all", comm.getAllDocuments)
+router.get("/all", comm.getAllDocuments);
+
+/** Retrieve documents in Communities that contain the prefix value
+ *  @route GET /api/comm/search/:query
+ *  @param query - String to use in the prefix search
+*/
+router.get("/search/:query", comm.prefixSearch);
 
 export default router;
