78449bb37b1845a822f323f9f1c93fbfcceb5ce7
vliu36 <116984041+vliu36@users.noreply.github.com>
2025-10-13 14:25:43 -0700

    added additional information to routes

--- server/routes/communities.ts
+++ server/routes/communities.ts
@@ -5,12 +5,14 @@ const router = express.Router();
 
 /** Retrieve all documents in Communities
  *  @route GET /api/comm/all
+ *  @returns Array containing JSON objects of all documents in the collection
  */
 router.get("/all", comm.getAllDocuments);
 
 /** Retrieve documents in Communities that contain the prefix value
  *  @route GET /api/comm/search/:query
  *  @param query - String to use in the prefix search
+ *  @returns Array containing JSON objects of documents with a matching prefix, or an empty array 
 */
 router.get("/search/:query", comm.prefixSearch);
 
