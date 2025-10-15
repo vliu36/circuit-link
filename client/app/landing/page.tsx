71a839bcc1c10f48bc24116911821e01e94cf6e4
HenryLiu114 <henryliudrake@gmail.com>
2025-10-14 23:12:29 -0700

    - Fixed formatting - Added Icons - Fixed Positioning of objects - (WIP) added a feature that changes the dimensions of the objects as the page's dimensions shrinks

--- client/app/landing/page.tsx
+++ client/app/landing/page.tsx
@@ -1,115 +1,61 @@
 "use client";
 import React from "react";
+import Styles from './landingPage.module.css';
 
 export default function Landing() {
     return (
         <div style={{ backgroundColor: "rgb(7, 17,45)", minHeight: "100vh" }}>
-            <style>{`
-            .Verticalline {
-                position: absolute;
-                top: 0;
-                bottom: 0;
-                width: 8px;
-                background-color: lightblue;
-                z-index: 0;
-            }
-            .Horizontalline {
-            position: relative;
-            margin-top: 20px;
-            margin-left: -10px;
-            width: 325px;
-            height: 8px;
-            background-color: lightblue;
-            z-index: 10;
-            }
-            .Left {
-            left: 25%;
-            }
-            .Right {
-            right: 25%;
-            }
-            .Searchbar {
-            text-align: center;
-            margin-top: 100px;
-            }
-            .Searchbar input {
-            background-color: black;
-            color: cyan;
-            border: 3px solid lightblue;
-            border-radius: 4px;
-            font-size: 12px;
-            width: 500px;
-            }
-            .Searchbar input::placeholder {
-            color: cyan;
-            }
-            .Buttoncontainer {
-            display: flex;
-            flex-direction: column;
-            align-items: flex-start;
-            gap: 10px;
-            margin-top: -450px;
-            margin-left: 50px;
-            }
-            .Button {
-            position: relative;
-            width: 200px;
-            height: 30px;
-            border: 4px;
-            color: gray;
-            text-align: center;
-            font-size: 12px;
-            transition-duration: 0.5s;
-            cursor: pointer;
-            }
-            .Button1, .Button2, .Button3, .Button4, .Button5 {
-            background-color: lightskyblue;
-            color: blue;
-            border: 2px solid;
-            }
-            .Button1:hover, .Button2:hover, .Button3:hover, .Button4:hover, .Button5:hover {
-            background-color: blue;
-            color: lightblue;
-            }
-        `}</style>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <h1 style={{ color: "lightblue", textAlign: "center" }}>
+        <div className = {Styles.Verticalline}></div>
+        <div className = {Styles.VerticallineOne}></div>
+        <div className = {Styles.VerticallineTwo}></div>
+        
+        <h1 className = {Styles.textBox}>
             Welcome to Circuit-Link,
         </h1>
-        <h2 style={{ color: "lightblue", textAlign: "center"}}>
+        <h2 className = {Styles.usernameTextBox}>
             User
         </h2>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
 
-        <div className="Searchbar">
+        <div className={Styles.Searchbar}>
             <input type="text" placeholder="Search..." />
         </div>
 
-        <div className="Buttoncontainer">
-            <button className="Button Button1">Home</button>
-            <button className="Button Button2">Friends</button>
-            <button className="Button Button3">Notifications</button>
-            <button className="Button Button4">Site News</button>
-            <button className="Button Button5">Log Out</button>
+        <div className={Styles.Buttoncontainer}>
+            <button className={Styles.ButtonStyle}>
+                <img src="/home.svg" className={Styles.homelogoBox}></img>
+                <div className = {Styles.buttonTextAlignment}>Home</div>   
+            </button>
+
+            <button className={Styles.ButtonStyle}>
+                <img src="/friends.svg" className={Styles.homelogoBox}></img>
+                <div className = {Styles.buttonTextAlignment}>Friends</div>
+            </button>
+
+            <button className={Styles.ButtonStyle}>
+                <img src="/notification.svg" className={Styles.homelogoBox}></img>
+                <div className = {Styles.buttonTextAlignment}>Notifications</div>
+            </button>
+
+            <button className={Styles.ButtonStyle}>
+                <img src="/news.svg" className={Styles.homelogoBox}></img>
+                <div className = {Styles.buttonTextAlignment}>Site News</div>
+            </button>
+
+            <button className={Styles.ButtonStyle}>
+                <img src="/logout.svg" className={Styles.homelogoBox}></img>
+                <div className = {Styles.buttonTextAlignment}>Log Out</div>
+            </button>
         </div>
 
-        <div className="Verticalline Left"></div>
-        <div className="Verticalline Right"></div>
-        <div className="Horizontalline"></div>
+        <div className={Styles.Left}></div>
+        <div className={Styles.Right}></div>
+        <div className={Styles.HorizontallineOne}></div>
+        <div className={Styles.Horizontalline}></div>
+        <div className = {Styles.logoBox}>
+            <img src="/circuitlinklogowback.svg"></img>
+        </div>
+        
+        <img src = "/add.svg" className = {Styles.addIcon}></img>
     </div>
     );
 }
