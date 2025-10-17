9e0b5961a06bc3aff4cf5e6ff22a122dde88ba71
vliu36 <116984041+vliu36@users.noreply.github.com>
2025-10-16 21:48:20 -0700

    fixed search result box

--- client/app/searchbar/searchbar.tsx
+++ client/app/searchbar/searchbar.tsx
@@ -45,7 +45,8 @@ export default function SearchBar() {
                     }
                     setSearchList(tempRes);
                 });
-            } else {
+            } 
+            else {
                 setSearchList([]);
             }
         }
