1f914fe5b1d4027e91a23c285872cfc2660e1bab
vliu36 <116984041+vliu36@users.noreply.github.com>
2025-10-16 18:42:01 -0700

    fixed key error

--- client/app/searchbar/searchResult.tsx
+++ client/app/searchbar/searchResult.tsx
@@ -5,7 +5,7 @@ export default function SearchResult({items}: any) {
     return (
         <ul> 
             {items?.map((item: any) => (
-                <li className={Styles.searchResult} key={item.index}>
+                <li className={Styles.searchResult} key={item}>
                     <Link href={`/community/${item}`}>{item}</Link>
                 </li>
             ))}
