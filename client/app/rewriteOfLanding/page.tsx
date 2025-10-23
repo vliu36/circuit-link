772d9484494234db2ccc24eebef15ab2ff38133c
HenryLiu114 <henryliudrake@gmail.com>
2025-10-22 21:04:18 -0700

    Made the new landing page (Check rewrite of landing)
    Keep old landing (there may be some things I ave missed)

--- client/app/rewriteOfLanding/page.tsx
+++ client/app/rewriteOfLanding/page.tsx
@@ -6,26 +6,89 @@ import { logout } from "./landing.ts";
 import SearchBar from "../searchbar/searchbar.tsx";
 import SearchResults from "../searchbar/searchResult.tsx";
 
-export default function rewriteOfLanding() {
-    const { user, userData, loading } = useAuth(); 
-
+export default function Landing() {
+    const { user, userData, loading } = useAuth();
+    
+    
+    
     return (
+       
         <div className = {Styles.background}>
-            <div className = {Styles.Verticalline}></div>
-            <div className={`${Styles.Verticalline} ${Styles.leftVLine}`}></div>
-            <div className={`${Styles.Verticalline} ${Styles.rightVLine}`}></div>
-            <div className = {`${Styles.horizontalLine} ${Styles.hline1}`}></div>
 
-            <div className = {Styles.containSearch}>
-                <div className = {Styles.textBox}>Welcome to Circuit-Link,</div>
-                <div className = {Styles.usernameTextBox}>
-                    henry{user?.displayName}
+            <div className = {Styles.yourCommunitiesBar}>
+                <h1>Your Communities</h1>
+                <button className = {Styles.communitiesButtons}>
+                    <img src = "plus.svg" className = {Styles.addIcon}></img>
+                    <h1 className = {Styles.buttonTextforCommunities}>Add a Community</h1>
+                </button>
+            </div>
+
+            <div className = {Styles.resourcesBar}>
+                <div className = {Styles.horizontalLine}></div>
+                <h1>Resources</h1>
+                <button className ={Styles.resourcesBarButtons}>
+                    <img src = "/aboutUs.svg" className = {Styles.aboutUsIcon}></img>
+                    <h1 className = {Styles.buttonText}>About Circuit Link</h1>
+                </button>
+                <button className ={Styles.resourcesBarButtons}>
+                    <img src = "/helpbutton.svg" className = {Styles.aboutUsIcon}></img>
+                    <h1 className = {Styles.buttonText}>Get Help</h1>
+                </button>
+                <button className ={Styles.resourcesBarButtons}>
+                    <img src = "/bug.svg" className = {Styles.aboutUsIcon}></img>
+                    <h1 className = {Styles.buttonText}>Report A Bug</h1>
+                </button>
+                <button className ={Styles.resourcesBarButtons}>
+                    <img src = "/rules.svg" className = {Styles.aboutUsIcon}></img>
+                    <h1 className = {Styles.buttonText}>Circuit Link Rules</h1>
+                </button>
+            </div>
+
+            <div className = {Styles.topUsersBar}>
+                <h1>Top Users</h1>
+            </div>
+
+            <div className = {Styles.topCommunitiesBar}>
+                <div className = {Styles.horizontalLine}></div>
+                <h1>Top Communities</h1>
+            </div>
+
+
+
+            <div className = {Styles.navBox}>
+                <div className = {Styles.homeLogo}>
+                    <img src="./circuitlinklogowback.svg"></img>
                 </div>
-                <div className = {Styles.searchBarPos}><SearchBar/></div>
-                <div className = {Styles.searchOptionsPos}> <SearchResults /></div>
+                <div className = {Styles.logInInfo}>
+                    <button>
+                        <img src = "./notifBell.svg" className = {Styles.notificationButton}></img>
+                    </button>
+                    <div className = {Styles.dropdown}>
+                        <button><img src = {user?.photoURL || "/circleUser.svg"} className = {Styles.settingsIcon}></img></button>
+                        <div className = {Styles.dropdownMenu}>
+                            <button onClick={() => window.location.href = "http://localhost:3000/profile"}>Profile</button>
+                            <button>Settings</button>
+                            <button onClick={logout}>Log Out</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className = {Styles.searchBarArea}>
+                <div className = {Styles.welcomeText}>
+                    Welcome to Circuit-Link,
+                </div>
+                <div className = {Styles.usernameText}>
+                    {user?.displayName}
+                </div>
+                <h3 className = {Styles.searchBarAlignment}>
+                    <SearchBar/>
+                </h3>
+                <h4> 
+                    <SearchResults />
+                </h4>
             </div>
         </div>
-        
         
     );
 }
