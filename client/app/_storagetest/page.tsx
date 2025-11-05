2c195109b07ed76d5baaa5ca9037bb67b0d97522
vliu36 <116984041+vliu36@users.noreply.github.com>
2025-11-04 17:17:02 -0800

    deprecated frontend implementation

---
+++ client/app/_storagetest/page.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import { useState } from "react";
+import { uploadImage } from "./_storageHandler.ts";
+
+export default function storageTest() {
+    const [image, setImage] = useState<FileList | null>();
+
+    const handleFileSubmission = (): void => {
+        try {
+            if (image) {
+                const file: File = image[0];
+                const cloudUrl = uploadImage(file).then((result) => {
+                    console.log(result);
+                })
+            }
+            else {
+                console.log("Filelist is empty");
+            }
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
+
+    return (
+        <div>
+            <p>Upload an Image</p>
+            <input 
+                type="file" 
+                id="file"
+                name="uploadedFile"
+                accept="image/png, image/jpeg" 
+                onChange={(e) => setImage(e.target.files)}
+            />
+            <button onClick={handleFileSubmission}>
+                Upload Image
+            </button>
+        </div>
+    )
+}
--- client/app/_storagetestOld/storageHandler.ts
+++ client/app/_storagetestOld/storageHandler.ts
@@ -1,6 +1,6 @@
 import { storage } from "@/app/_firebase/firebase.ts";
 import { getBytes, ref, uploadBytes } from "firebase/storage";
-import ffmpeg from "fluent-ffmpeg";
+// import ffmpeg from "fluent-ffmpeg";
 import path from "path";
 
 // Takes an image and uploads it to the cloud bucket
@@ -31,30 +31,30 @@ export async function uploadVideo(file: File) {
         if (file.size > 5000000) {
             throw Error("File size exceeds 5 MB!");
         }
-        await convertVideo(path.resolve(file.name)).then((result) => {
+        // await convertVideo(path.resolve(file.name)).then((result) => {
 
-        })
+        // })
     }
     catch (err) {
         console.error(err);
     }
 }
 
-function convertVideo(filepath: string) {
-    return new Promise<void>((resolve, reject) => {
-        ffmpeg(filepath)
-            .outputOptions("-vf", "scale=-1:360")
-            .on("end", () => {
-                console.log("Video processing finished successfully")
-                resolve();
-            })
-            .on("error", (err) => {
-                console.log(err);
-                reject(err);
-            })
-            .save(filepath);
-    });
-}
+// function convertVideo(filepath: string) {
+//     return new Promise<void>((resolve, reject) => {
+//         ffmpeg(filepath)
+//             .outputOptions("-vf", "scale=-1:360")
+//             .on("end", () => {
+//                 console.log("Video processing finished successfully")
+//                 resolve();
+//             })
+//             .on("error", (err) => {
+//                 console.log(err);
+//                 reject(err);
+//             })
+//             .save(filepath);
+//     });
+// }
 
 // Download a video or image from the cloud bucket
 // path: string - The relative path of the media in the firebase cloud storage bucket
