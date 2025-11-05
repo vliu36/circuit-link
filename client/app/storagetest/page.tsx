0092198977717573fecccd78b9dbcafe8ed205ea
vliu36 <116984041+vliu36@users.noreply.github.com>
2025-11-04 16:18:55 -0800

    moved storage implementation into temp page to avoid conflicts

---
+++ client/app/storagetest/page.tsx
@@ -0,0 +1,11 @@
+
+export default function storageTest() {
+    return (
+        <input 
+            type="file" 
+            id="file"
+            name="uploadedFile"
+            accept="image/png, image/jpeg" 
+        />
+    )
+}
