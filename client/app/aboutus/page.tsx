d1987c1e32a12c28b05d2e7938df922952a4f04f
Alejandro Manzo <alejandromanzo2003@gmail.com>
2025-11-16 20:50:16 -0800

    Massive UI overhaul, hopefully edwin likes it

--- client/app/aboutus/page.tsx
+++ client/app/aboutus/page.tsx
@@ -34,8 +34,8 @@ export default function AboutUs() {
                     Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah
                 </h2>
             </div>
-
-            <Link className = {Styles.returnButton} href = "./landing" replace>
+{/*Added http://localhost:3000 instead of ./landing since we got rid of it*/}
+            <Link className = {Styles.returnButton} href = "http://localhost:3000" replace>
                 <h1 className = {Styles.returnButtonText}>Return</h1>
             </Link>
         </div>
