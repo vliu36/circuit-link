7766e7f929010fc276d11882f9be4c1ea0c70f8c
HenryLiu114 <henryliudrake@gmail.com>
2025-11-16 16:36:14 -0800

    Changed ui to grids

--- client/app/community/[commName]/[forumSlug]/[postId]/page.tsx
+++ client/app/community/[commName]/[forumSlug]/[postId]/page.tsx
@@ -266,7 +266,7 @@ export default function PostDetail({ params }: { params: Promise<{ commName: str
     return (
         
         <div className = {styles.background}>
-            <div className = {styles.yourCommunitiesBar}>
+            <div className = {styles.yourCommunitiesBar} style={{gridArea: "CommunitiesBar"}}>
                 <h1>Your Communities</h1>
                 <button className = {styles.communitiesButtons}>
                     <img src = "plus.svg" className = {styles.addIcon}></img>
@@ -275,7 +275,7 @@ export default function PostDetail({ params }: { params: Promise<{ commName: str
             </div>
 
             
-            <div className = {styles.serverBar}>
+            <div className = {styles.serverBar} style={{gridArea: "ServerBar"}}>
                 <div className = {styles.horizontalLine}></div>
                 <h1>{commName}</h1>
                 <div className = {styles.horizontalLine}></div>
@@ -287,13 +287,13 @@ export default function PostDetail({ params }: { params: Promise<{ commName: str
                 </div>
             </div>
 
-            <div className = {styles.channelInfoBox}>
+            <div className = {styles.channelInfoBox} style={{gridArea: "RightBar"}}>
                 <div className = {styles.channelInfoh1}>{commName}</div>
                 <div className = {styles.channelInfoh2}>{community?.description}</div>
                 
             </div>
             
-            <div className = {styles.RulesBar}>
+            <div className = {styles.RulesBar} style={{gridArea: "RightBar"}}>
                 <div className = {styles.horizontalLine}></div>
                 <div className = {styles.horizontalLine}></div>
                 <h1>Rules</h1>
@@ -317,14 +317,14 @@ export default function PostDetail({ params }: { params: Promise<{ commName: str
                 </div>
             </div>
 
-            <div className = {styles.navBox}>
+            <div className = {styles.navBox} style={{gridArea: "NavBar"}}>
                 <NavBar/>
             </div>
             
             
             
 
-            <div className={styles.postsPage}>
+            <div className={styles.postsPage} style={{gridArea: "Center"}}>
                 <div className={styles.backDisplay}>
                     <Link href={`/community/${commName}/${forumSlug}`}>
                         Back
