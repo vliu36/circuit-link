503c91610cfb9d2b5a8ae2e2b658022e1fa468ff
Alejandro Manzo <alejandromanzo2003@gmail.com>
2025-11-29 18:10:26 -0800

    Resource pages have been fixed and re-vamped.

--- client/app/help/page.tsx
+++ client/app/help/page.tsx
@@ -1,24 +1,51 @@
 "use client";
 import React from "react";
 import Styles from "./help.module.css";
-import Link from 'next/link';
+import Link from "next/link";
 
-export default function Help() {{/*function RulesPage*/}
+export default function Help() {
     
     return (
         <div className = {Styles.background}>
-            {/*Added http://localhost:3000 instead of ./landing since we got rid of it*/}
-            <Link className = {Styles.returnButton} href = "http://localhost:3000" replace>
-                <h1 className = {Styles.returnButtonText}>Return</h1>
-            </Link>
-            <div className = {Styles.mainBox}>
-                <div className = {Styles.titleBox}>
-                    <h1 className={Styles.titleText}>Help</h1>
-                    <div className = {Styles.titleBoxLine}></div>
-                </div>
-                <div className = {Styles.innerBox}>
-                    <p>Will implement a drop down feature.</p>
-                </div>
+            <div className = {Styles.pageDoc}>
+                <h1 className = {Styles.heading1}>Help</h1>
+
+                <section>
+                    <p className = {Styles.paragraph}>
+                    <strong>Landing Page</strong>: First page you see when logging in or upon registering. Is also the first before account creation.
+                    </p>
+                    <p className = {Styles.paragraph}>
+                    <strong>Login Page</strong>: Page a user is taken to when trying to sign in with their created account.
+                    </p>
+                    <p className = {Styles.paragraph}>
+                    <strong>Register Page</strong>: Page a user selects if they have yet to create their account. Cannot access full features without an account.
+                    </p>
+                    <p className = {Styles.paragraph}>
+                    <strong>Communities</strong>: A hub-like place for people to join and dicuss or share things about topics they are passionate about or interested in.
+                    </p>
+                    <p className = {Styles.paragraph}>
+                    <strong>Forums</strong>: Sub communities in which people can specify about certain things within their shared interests. Great way to get specific.
+                    </p>
+                    <p className = {Styles.paragraph}>
+                    <strong>Moderator</strong>: A person who has special privilages in the community or forums. Can delete posts, replies, and even ban people when necessary.
+                    </p>
+                    <p className = {Styles.paragraph}>
+                    <strong>Member</strong>: A person who is by all means regular in the sense that they do not have special privilages like moderators. Can post, reply, and like.
+                    </p>
+                    <p className = {Styles.paragraph}>
+                    <strong>Friends</strong>: People who you send a friend request to and accept. Typically share similar interests and met in a community both are part of.
+                    </p>
+                    <p className = {Styles.paragraph}>
+                    <strong>Yays</strong>: Yays are your traditional likes. Members of forums and communities can yay posts they like.
+                    </p>
+                    <p className = {Styles.paragraph}>
+                    <strong>Nays</strong>: Nays are the polar opposite of Yays and behave like dislikes. Members can Nay posts that they dislike.
+                    </p>
+                </section>
+                
+                <Link className = {Styles.returnButton} href = ".." replace>
+                    <span className = {Styles.returnButtonText}>Return</span>
+                </Link>
             </div>
         </div>
     );
--- client/app/siterules/page.tsx
+++ client/app/siterules/page.tsx
@@ -7,18 +7,33 @@ export default function SiteRules() {
     
     return (
         <div className = {Styles.background}>
-            <Link className = {Styles.returnButton} href = ".." replace>
-                <h1 className = {Styles.returnButtonText}>Return</h1>
-            </Link>
-            <div className = {Styles.rulesBox}>
-                <div className = {Styles.titleBox}>
-                    <h1 className={Styles.titleText}>Circuit Link Rules</h1>
-                    <div className = {Styles.titleBoxLine}></div>
-                </div>
-                <div className = {Styles.innerBox}>
-                    <p>0. Please be nice, don&apos;t be rude.
-                    The first rule of Circuit Link is to never talk about Circuit Link.</p>
-                </div>
+            <div className = {Styles.pageDoc}>
+                <h1 className = {Styles.heading1}>Site Rules</h1>
+
+                <section>
+                    <p className = {Styles.paragraph}>
+                    <strong>1.</strong> Do not post things you would not want other to see. You do you bro, its on you.
+                    </p>
+                    <p className = {Styles.paragraph}>
+                    <strong>2.</strong> Be smart. Do not share personal information that you do not want floating around the internet.
+                    </p>
+                    <p className = {Styles.paragraph}>
+                    <strong>3.</strong> Read the room. If you are having issues with people online...that you have no idea of...just walk away.
+                    </p>
+                    <p className = {Styles.paragraph}>
+                    <strong>4.</strong> Be yourself and make sure to enjoy yourself.
+                    </p>
+                    <p className = {Styles.paragraph}>
+                    <strong>5.</strong> In the event that you see something seriously wrong, please report it.
+                    </p>
+                    <p className = {Styles.paragraph}>
+                    <strong>6.</strong> Have fun!
+                    </p>
+                </section>
+                
+                <Link className = {Styles.returnButton} href = ".." replace>
+                    <span className = {Styles.returnButtonText}>Return</span>
+                </Link>
             </div>
         </div>
     );
